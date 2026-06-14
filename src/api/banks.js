f9063a9b20ae35965656a5653a7e167cc6d012de
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-06-14 18:53:30 +0400

    feature: bank logo functionality

--- src/api/banks.js
+++ src/api/banks.js
@@ -5,3 +5,11 @@ export const getBanks = () => apiClient.get('/banks');
 export const createBank = (name) => apiClient.post('/banks', { name });
 
 export const deleteBank = (id) => apiClient.delete(`/banks/${id}`);
+
+export const uploadBankLogo = (id, file) => {
+    const formData = new FormData();
+    if (file) {
+        formData.append('file', file);
+    }
+    return apiClient.post(`/banks/${id}/upload-photo`, formData, {headers: { 'Content-Type': 'multipart/form-data' },});
+};
